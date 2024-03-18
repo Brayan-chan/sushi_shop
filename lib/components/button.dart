9112f7b9f3b8f04260018523a442e5d434533a26
Brayan-chan <al071392@uacam.mx>
2024-03-18 02:50:39 -0600

    Dandole estilos y un icono al boton

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+//Los componentes se utilizan para crear elementos de interfaz de usuario
 class MyButton extends StatelessWidget {
   final String text;
   const MyButton({
@@ -10,8 +11,28 @@ class MyButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(color: Color.fromARGB(212, 135, 81, 77)),
-      child: Text(text),
+      //Asignando estilos y borde al boton
+      decoration: BoxDecoration(color: Color.fromARGB(212, 135, 81, 77), borderRadius: BorderRadius.circular(40)),
+
+      //Definiendo el tamaño del boton
+      //EdgeInsts sirve para definir el margen del boton
+      padding: EdgeInsets.all(20),
+      child: Row(
+        //Alineando el contenido del boton al centro
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //Texto
+          Text(
+            text,
+            style: TextStyle(color: Colors.white),
+          //Definiendo estilos del texto dentro del boton  
+          ),
+
+          const SizedBox(width: 10),
+          //Icono
+          Icon(Icons.arrow_forward, color: Colors.white,),
+        ],
+      ),
     );
   }
 }
