e695d6ac9db885a3ff06ad957de45a77028d87c8
Brayan-chan <al071392@uacam.mx>
2024-03-18 01:44:32 -0600

    Definicion de background y columna de informacion

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -5,6 +5,24 @@ class IntroPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      //Se asigna un color a la pantalla
+      backgroundColor: Color.fromARGB(255, 138, 60, 55),
+      //Creamos una columna, simulando un grid que contiene diferentes elementos
+      body: Column(
+        children: [
+          //Nombre de la compania
+
+          //Logo de la compania
+
+          //Titulo
+
+          //Subtitulo
+
+          //Boton Iniciar o Get Started
+
+        ],
+      ),
+    );
   }
 }
